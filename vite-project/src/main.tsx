974db0b8543e7351f074f7700743d8072aa48e3d
vuturerfernanades <roger.fernandes@vutu.re>
2025-01-18 23:12:08 +0530

    Form Editing working

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
-import './index.css'
-import App from './App.tsx'
+import './app/layouts/styles.css'
+import App from './app/layouts/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
