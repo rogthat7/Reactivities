151eb59c936c3acea1129dbb75b4e46fb7abc9a6
rogthat7 <rogthat7@gmail.com>
2025-02-01 22:32:05 +0530

    End of Section 9 Styling

--- vite-project/src/features/activities/dashboard/ActivityListItem.tsx
+++ vite-project/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import { useState, SyntheticEvent } from "react";
 import { Link } from "react-router-dom";
-import { Item, Button, Label } from "semantic-ui-react";
+import { Item, Button, Label, Segment, Icon } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { Activity } from "../../../app/models/activity";
 
@@ -19,27 +19,49 @@ export default function ActivityListItem({ activity }: Props) {
         deleteActivity(id);
     }
     return (
-        <Item key={activity.id}>
-            <Item.Content>
-                <Item.Header as='a'>{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                    <div>{activity.description}</div>
-                    <div>{activity.city},{activity.venue}</div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button as={Link} to={`/activities/${activity.id}`} floated="right" content='View' color="blue" />
-                    <Button
-                        name={activity.id}
-                        loading={loading && target === activity.id}
-                        onClick={(e) => handleActivityDelete(e, activity.id)}
-                        floated="right"
-                        content='Delete'
-                        color="red"
-                    />
-                    <Label basic content={activity.category} />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                            {activity.isHost &&
+                                <Item.Description>
+                                    <Label basic color='orange'>
+                                        You are hosting this activity
+                                    </Label>
+                                </Item.Description>}
+                            {activity.isGoing &&
+                                <Item.Description>
+                                    <Label basic color='green'>
+                                        You are going to this activity
+                                    </Label>
+                                </Item.Description>}
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
     )
 }
