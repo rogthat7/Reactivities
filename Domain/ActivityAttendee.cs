f764a582d49931a269bac3bba03514c06c3779d1
rogthat7 <rogthat7@gmail.com>
2025-04-22 21:46:19 +0530

    Section 14 Infrastructure Project Added

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public Guid ActivityId { get; set; }
+        public Activity Activity { get; set; }
+        public bool Host { get; set; }
+        public DateTime DateJoined { get; set; } = DateTime.UtcNow;
+    }
+}
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,6 +9,21 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Activity> Activities { get; set; }
+        public required DbSet<Activity> Activities { get; set; }
+        public required DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ActivityAttendee>(x=>x.HasKey(a=> new {a.ActivityId,a.UserId}));
+            builder.Entity<ActivityAttendee>()
+            .HasOne(x=>x.User)
+            .WithMany(x=>x.Activities)
+            .HasForeignKey(x=>x.UserId);
+
+            builder.Entity<ActivityAttendee>()
+            .HasOne(x=>x.Activity)
+            .WithMany(x=>x.Attendees)
+            .HasForeignKey(x=>x.ActivityId);
+        }
     }
 }
