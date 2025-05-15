ae0b2713a0daa9f22b4d129f6793843b99744831
rogthat7 <rogthat7@gmail.com>
2025-05-15 09:30:03 +0530

    Node version update test

---
+++ API/wwwroot/assets/react-popper-CP2YiNjc.js
@@ -0,0 +1,1 @@
+import"./react-BqlbiFCO.js";import"./react-dom-Cern445X.js";
--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -14,6 +14,13 @@ export default defineConfig({
         }
         warn(warning);
       },
+      output:{
+        manualChunks(id) {
+            if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+        }
+    }
     },
   },
   server:{
